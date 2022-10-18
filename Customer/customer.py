4cc3c4613927e4e332a036ab7be2409730a494e4
Nassor JA <rossan1tech@gmail.com>
2022-10-18 22:13:56 +0300

    Addition of email field to customer and input validation

--- Customer/customer.py
+++ Customer/customer.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 def customer_info():
@@ -71,14 +72,26 @@ def add_customer():
             break
         else:
             print("\nINVALID INPUT! Please enter 1 or 2!")
+    while True:
+        mail = input("\nEnter customer's email:")
+        confirm_format = checky(mail)
+        if confirm_format == 'y':
+            cus_data["Email"] = mail
+            break
+        else:
+            print("\nINVALID EMAIL FORMAT! Email format must be of: \"username@company.domain\"")
+            continue
 
     while True:
-        try:
-            cus_data["Phone_Number"] = int(input("\nEnter customer's phone number: "))
-        except ValueError:
-            print("\nINVALID INPUT! Customer's phone number can't be an alphabet")
+        ftel_in = input("\nEnter customer's phone number:  ")
+        confirm_pformat = check_numformat(ftel_in)
+        if confirm_pformat == 'y':
+            cus_data["Phone_Number"] = ftel_in
+            break
+        else:
+            print("\nINVALID PHONE NUMBER FORMAT! Enter values (0-9) and 10 digits long!")
             continue
-        break
+
     temp.append(cus_data)
     with open("Customer/cus.json", "w") as json_file:
         json.dump(temp, json_file, indent=4)
@@ -92,9 +105,10 @@ def view_customer():
         for entry in temp:
             cus_name = entry["Customer_Name"]
             gender = entry["Gender"]
+            mail = entry["Email"]
             tel = entry["Phone_Number"]
             print(f"CustomerID: {i}")
-            print(f"Name: {cus_name}, Gender: {gender}, Phone number: {tel}")
+            print(f"Name: {cus_name}, Gender: {gender}, Email: {mail}, Phone number: {tel}")
             i = i + 1
 
 
@@ -120,7 +134,7 @@ def delete_customer():
         for entry in temp:
             if i == int(delete_opt):
                 pass
-                i = i+1
+                i = i + 1
             else:
                 sieved_data.append(entry)
                 i = i + 1
@@ -154,13 +168,14 @@ def update_customer():
             if i == int(update_opt):
                 cus_name = entry["Customer_Name"]
                 gender = entry["Gender"]
+                mail = entry["Email"]
                 tel = entry["Phone_Number"]
-                print(f"Name: {cus_name}")
+                print(f"\nName: {cus_name}")
                 cus_name = input("Enter updated customer's name: ")
-                print(f"Gender: {gender}")
+                print(f"\nGender: {gender}")
                 while True:
                     try:
-                        gender_rec = int(input("\nEnter 1 if customer gender is Male and 2 if Female: "))
+                        gender_rec = int(input("Enter 1 if customer gender is Male and 2 if Female: "))
                     except ValueError:
                         print(f"\nINVALID INPUT! Entered value can't be an Alphabet")
                         continue
@@ -172,16 +187,28 @@ def update_customer():
                         break
                     else:
                         print("\nINVALID INPUT!")
-                print(f"Phone number: {tel}")
+                print(f"\nEmail: {mail}")
                 while True:
-                    try:
-                        tel = int(input("\nEnter updated customer's phone number: "))
-                        print("\n")
-                    except ValueError:
-                        print("\nINVALID INPUT! Customer's phone number can't be an alphabet")
+                    mail_in = input("Enter updated customer's email:")
+                    confirm_format = checky(mail_in)
+                    if confirm_format == 'y':
+                        mail = mail_in
+                        break
+                    else:
+                        print("\nINVALID EMAIL FORMAT! Email format must be of: \"username@company.domain\"")
                         continue
-                    break
-                updated_cus_list.append({"Customer_Name": cus_name, "Gender": gender, "Phone_Number": tel})
+                print(f"\nPhone number: {tel}")
+                while True:
+                    tel_in = input("Enter updated customer's phone number: ")
+                    confirm_pformat = check_numformat(tel_in)
+                    if confirm_pformat == 'y':
+                        tel = tel_in
+                        break
+                    else:
+                        print("\nINVALID PHONE NUMBER FORMAT! Enter values (0-9) and 10 digits long!")
+                        continue
+                updated_cus_list.append({"Customer_Name": cus_name, "Gender": gender, "Email": mail,
+                                         "Phone_Number": tel})
                 i = i + 1
             else:
                 updated_cus_list.append(entry)
@@ -266,4 +293,20 @@ def confirm_cuspnum(cus_conphone):
     return
 
 
+# Email format validation
+def checky(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if re.fullmatch(regex, email):
+        return 'y'
+    else:
+        return
+
+
+# number format validation
+def check_numformat(telno):
+    regex = r'^[01 | 07]{2}[0-9]{8}$'
+    if re.fullmatch(regex, telno):
+        return 'y'
+    else:
+        return
 # customer_info()
